0863011ea57586b6bc1fd1636e5e71773ff2116a
Linus Dietz <linus.dietz@tum.de>
2017-08-25 14:04:37 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Region Tree of the World
+
+[![Build Status](https://travis-ci.com/lynyus/geonames-region-tree.svg?token=3xSpCxqKzHnH2NXbgTwq&branch=master)](https://travis-ci.com/lynyus/geonames-region-tree)
+
+Builds a region tree using the [GeoNames API](http://www.geonames.org).
+
+The four levels of the tree are Earth -> continents -> countries -> states.
+At the time of this writing it returned 7 continents, 250 countries and 3874 federal states/sub-regions of countries.
+
+
+## Usage
+
+The usage is intended to be as a library, the main class is [`regiontree.region_tree.RegionTree`](regiontree/region_tree.py).
+
+
+
+## License
+
+This library is licensed under the MIT license. See the [LICENSE.md](LICENSE.md) for the full MIT license.
--- test/test_region_tree.py
+++ test/test_region_tree.py
@@ -12,18 +12,18 @@ class TestRegionTree(unittest.TestCase):
     def test_number_of_regions(self):
         geonames_user = os.environ['GEONAMES_KEY']
 
-        world_tree = RegionTree(GeonamesAPI(0.0001, logging, geonames_user))
+        world_tree = RegionTree(GeonamesAPI(0.1, logging, geonames_user))
 
         expected_num_continents = 7
         expected_num_countries = 250
-        expected_num_regions = 3874
+        expected_num_states = 3874
         num_countries = 0
-        num_regions = 0
+        num_states = 0
         for continent in world_tree.tree:
             for country in continent:
                 num_countries += 1
-                num_regions += len(country)
+                num_states += len(country)
 
         self.assertEqual(expected_num_continents, len(world_tree.tree))
         self.assertEqual(expected_num_countries, num_countries)
-        self.assertEqual(expected_num_regions, num_regions)
+        self.assertEqual(expected_num_states, num_states)
