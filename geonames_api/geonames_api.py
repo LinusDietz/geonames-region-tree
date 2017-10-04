5ed86a3e099da25d8a35b3339ec22cbce393b627
Linus Dietz <linus.dietz@tum.de>
2017-10-04 17:30:34 +0200

    Throttle is reconsolidated, tests for region numbers splitted

--- geonames_api/geonames_api.py
+++ geonames_api/geonames_api.py
@@ -11,50 +11,57 @@ class GeonamesAPI:
         self.base_url = 'http://api.geonames.org/'
         self.throttle = throttle
         self.logging = logging
+        if user is None or user == '':
+            raise ValueError("No geonames user provided! Set an environment variable using export GEONAMES_KEY=<user>")
         self.user = user
 
-    def do_id_api_call(self, api: str, geoname_id: int) -> tuple:
+    def call_geonames(self, request) -> dict:
         sleep(self.throttle)
-        raw_dict = xmltodict.parse(urllib.request.urlopen(self.base_url + "%s?geonameId=%s&username=%s" % (api, geoname_id, self.user)).read())
-        return self.log_status_message(raw_dict), raw_dict
+        return xmltodict.parse(urllib.request.urlopen(self.base_url + request).read())
+
+    def do_id_api_call(self, api: str, geoname_id: int) -> dict:
+        raw_dict = self.call_geonames("%s?geonameId=%s&username=%s" % (api, geoname_id, self.user))
+        self.log_status_message(raw_dict)
+        return raw_dict
 
     def children(self, geoname_id: int) -> set():
         result = set()
-        failure, raw_result = self.do_id_api_call('children', geoname_id)
+        try:
+            raw_result = self.do_id_api_call('children', geoname_id)
 
-        if failure or raw_result['geonames']['totalResultsCount'] == '0':
+            if raw_result['geonames']['totalResultsCount'] == '0':
+                return result
+
+            if raw_result['geonames']['totalResultsCount'] == '1':
+                result.add(Region(int(raw_result['geonames']['geoname']['geonameId']),
+                                  raw_result['geonames']['geoname']['name'],
+                                  raw_result['geonames']['geoname']['toponymName'],
+                                  Coordinate(raw_result['geonames']['geoname']['lat'], raw_result['geonames']['geoname']['lng'])
+                                  ))
+            else:
+                for child in raw_result['geonames']['geoname']:
+                    result.add(Region(int(child['geonameId']), child['name'], child['toponymName'], Coordinate(child['lat'], child['lng'])))
+
             return result
-
-        if raw_result['geonames']['totalResultsCount'] == '1':
-            result.add(Region(int(raw_result['geonames']['geoname']['geonameId']),
-                              raw_result['geonames']['geoname']['name'],
-                              raw_result['geonames']['geoname']['toponymName'],
-                              Coordinate(raw_result['geonames']['geoname']['lat'], raw_result['geonames']['geoname']['lng'])
-                              ))
-        else:
-            for child in raw_result['geonames']['geoname']:
-                result.add(Region(int(child['geonameId']), child['name'], child['toponymName'], Coordinate(child['lat'], child['lng'])))
-
-        return result
+        except KeyError:
+            return set()
 
     def name(self, geoname_id: int) -> dict:
-        return self.do_id_api_call('get', geoname_id)[1]
+        return self.do_id_api_call('get', geoname_id)
 
-    def log_status_message(self, raw_dict) -> bool:
+    def log_status_message(self, raw_dict):
         try:
             status_message = raw_dict['geonames']['status']['@message']
             if status_message.startswith('no children for'):
                 self.logging.info(status_message)
-                return True
             else:
                 self.logging.warning(status_message)
-                return True
         except KeyError:
-            return False
+            pass
 
     def reverse_region(self, point: Coordinate) -> str:
 
-        raw_dict = xmltodict.parse(urllib.request.urlopen(f"{self.base_url}countrySubdivision?lat={point.latitude}&lng={point.longitude}&username={self.user}"))
+        raw_dict = self.call_geonames(f"countrySubdivision?lat={point.latitude}&lng={point.longitude}&username={self.user}")
         try:
             return raw_dict['geonames']['countrySubdivision']['adminName1']
         except KeyError:
@@ -65,7 +72,7 @@ class GeonamesAPI:
                 return None
 
     def reverse_region_geoname_id(self, point: Coordinate) -> int:
-        raw_dict = xmltodict.parse(urllib.request.urlopen(f"{self.base_url}countrySubdivision?lat={point.latitude}&lng={point.longitude}&username={self.user}"))
+        raw_dict = self.call_geonames(f"countrySubdivision?lat={point.latitude}&lng={point.longitude}&username={self.user}")
 
         try:
             return self.geoname_id_of_region(raw_dict['geonames']['countrySubdivision']['adminName1'])
@@ -77,13 +84,13 @@ class GeonamesAPI:
                 return 0
 
     def geoname_id_of_region(self, name: str, ) -> int:
-        raw_dict = xmltodict.parse(urllib.request.urlopen(f"{self.base_url}search?q={name.replace(' ', '-')}&maxRows=10&username={self.user}"))
+        raw_dict = self.call_geonames(f"search?q={name.replace(' ', '-')}&maxRows=10&username={self.user}")
         for result in raw_dict['geonames']['geoname']:
             if result['fcl'] == 'A' and result['fcode'] == 'ADM1':
                 return int(result['geonameId'])
 
     def geoname_id_of(self, name: str, ) -> int:
-        raw_dict = xmltodict.parse(urllib.request.urlopen(f"{self.base_url}search?q={name.replace(' ', '-')}&maxRows=1&username={self.user}"))
+        raw_dict = self.call_geonames(f"search?q={name.replace(' ', '-')}&maxRows=1&username={self.user}")
         try:
             return int(raw_dict['geonames']['geoname']['geonameId'])
         except KeyError:
--- test/test_region_tree.py
+++ test/test_region_tree.py
@@ -1,3 +1,4 @@
+import difflib
 import logging
 import os
 import unittest
@@ -10,20 +11,23 @@ class TestRegionTree(unittest.TestCase):
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
     world_tree = RegionTree(GeonamesAPI(0.1, logging, os.environ['GEONAMES_KEY']))
 
-    def test_number_of_regions(self):
-        expected_num_continents = 7
-        expected_num_countries = 250
-        expected_num_states = 3874
-        num_countries = 0
-        num_states = 0
-        for continent in self.world_tree.tree:
-            for country in continent:
-                num_countries += 1
-                num_states += len(country)
-
-        self.assertEqual(expected_num_continents, len(self.world_tree.tree))
-        self.assertEqual(expected_num_countries, num_countries)
-        self.assertEqual(expected_num_states, num_states)
+    def test_number_of_continents(self):
+        self.assertEqual(7, len(self.world_tree.tree))
 
     def test_get_countries(self):
         self.assertEqual(250, len(self.world_tree.get_countries()))
+
+    def test_number_of_regions(self):
+        expected_num_states = 3874
+        num_states = 0
+        for continent in self.world_tree.tree:
+            for country in continent:
+                num_states += len(country)
+
+        self.assertEqual(expected_num_states, num_states)
+
+    def test_region_info(self):
+        self.world_tree.print_region_tree('./resources/world_tree_new.txt')
+        with open('./resources/world_tree.txt', 'r', encoding="utf-8") as old:
+            with open('./resources/world_tree_new.txt', 'r', encoding="utf-8") as new:
+                print(''.join(difflib.ndiff(sorted([l.strip() + '\n' for l in old.readlines()]), sorted([l.strip() + '\n' for l in new.readlines()]))))
